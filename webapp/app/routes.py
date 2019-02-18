c33c1327eddc6b9cbf1b7676bb06980e9df946bc
Alex Rosa <h.rosa822@gmail.com>
2019-02-18 13:07:35 -0700

    add functionality for login form

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 import datetime
 from app.forms import LoginForm
 
@@ -30,7 +30,11 @@ def index():
 
     return  render_template('index.html', title = 'Arctic Spider', \
                  lastCrawl = lastCrawl, data=data)
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data,form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html',title='Sign In',form = form)
