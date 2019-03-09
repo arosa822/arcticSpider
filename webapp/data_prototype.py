21c12a6539c85a644a8ac867b0a41b2bc4dadfc4
arosa822 <h.rosa822@gmail.com>
2019-03-09 10:16:53 -0700

    add method for selecting most recent entry to vault

--- webapp/data_prototype.py
+++ webapp/data_prototype.py
@@ -1,6 +1,7 @@
 from app import db
 from app.models import User,Resort,Conditions
 import os
+import re
 
 DIR = '../vault'
 
@@ -16,14 +17,24 @@ def addResort(location):
     print(r)
 
 def searchDir(directory):
+    '''This fuction performs a search on files 
+    in the vault, filters the resulting list
+    to contain only those which have been assigned 
+    a number, then selects the most recent addition
+    
+    :param str directory: relative directory to search
+    :return str results[0]: most recent crawl result
+    '''
     results = []
     for root, dirs, files in os.walk(directory):
         for filename in files:
             if filename.endswith('.pickle'):
-                print(filename)
                 results.append(filename)
 
-    return results
+    #only return files with a number associated
+    results = list(filter(lambda x: bool(re.search(r'\d',x)),results))
+    results.sort(reverse=True)
+    return results[0]
 
 def main():
     addUser('alex','alex.com')
